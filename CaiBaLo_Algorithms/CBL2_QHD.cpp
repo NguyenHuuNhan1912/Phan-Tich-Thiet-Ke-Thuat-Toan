0686e85143ad789b924986a48ada9c700d81aa93
Nguyen Huu Nhan <82103041+NguyenHuuNhan1912@users.noreply.github.com>
2021-05-26 10:18:48 +0700

    CBL2_QHD
    C code

---
+++ CaiBaLo_Algorithms/CBL2_QHD.cpp
@@ -0,0 +1,109 @@
+//Cai ba lo quy hoach dong (Cai ba lo 2)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct {
+	char TenDV[20];
+	int SL, TL, GT, PA;
+} DoVat;
+
+typedef int bang[50][100];
+
+DoVat *ReadFromFile(int *W, int *n){
+     FILE *f;
+     f = fopen("QHD_CaiBalo2.INP", "r");
+     fscanf(f, "%d",W);
+	 DoVat *dsdv;
+	 dsdv=(DoVat*)malloc(sizeof(DoVat));
+	 int i=0;
+ 	 while (!feof(f)){
+	   fscanf(f, "%d%d%d%[^\n]",&dsdv[i].TL,&dsdv[i].GT,&dsdv[i].SL,&dsdv[i].TenDV);
+	   dsdv[i].PA=0;
+	   i++;
+	   dsdv=(DoVat*)realloc(dsdv,sizeof(DoVat)*(i+1));  
+	 }
+	 *n=i;
+     fclose(f);
+     return dsdv;
+}
+
+void InDSDV(DoVat *dsdv ,int n, int W){
+	int i, TongTL=0, TongGT=0;
+	printf("\nPhuong an Cai Ba lo 2 dung thuat toan QUY HOACH DONG nhu sau:\n");
+	printf("+---+------------------+-------------+---------+----------+-----------+\n");
+	printf("|STT|     Ten Do Vat   | Trong Luong | Gia Tri | So Luong | Phuong an |\n");
+	printf("+---+------------------+-------------+---------+----------+-----------+\n");
+	for(i=0;i<n;i++){
+		printf("|%2d |%-18s|%8d     |%5d    |%5d     |%6d     |\n",i+1,dsdv[i].TenDV,dsdv[i].TL,dsdv[i].GT,dsdv[i].SL,dsdv[i].PA);
+		TongTL=TongTL+dsdv[i].PA * dsdv[i].TL;
+		TongGT=TongGT+dsdv[i].PA * dsdv[i].GT;
+	}	
+	printf("+---+------------------+-------------+---------+----------+-----------+\n");	
+	printf("Trong luong cua ba lo = %-9d\n",W);
+	printf("Tong trong luong = %-9d\n",TongTL);
+	printf("Tong gia tri = %-9d\n",TongGT);
+	printf("Phuong an: X(");
+	for(i=0;i<n-1;i++){
+		printf("%d, ",dsdv[i].PA);
+	}
+	printf("%d)",dsdv[n-1].PA);
+}
+int min(int a, int b){
+	if(a<b) return a;
+	else return b;
+}
+void TaoBang(DoVat *dsdv,int n,int W, bang F,bang X){
+ 	int xk, yk, k;
+ 	int FMax, XMax, V;
+ 	for(V= 0; V<=W; V++) {
+ 		X[0][V] = min(dsdv[0].SL,V/dsdv[0].TL);
+ 		F[0][V] = X[0][V] * dsdv[0].GT;
+ 	}
+	 for(k= 1; k<n; k++)
+		 for(V=0; V<=W; V++) {
+			 FMax = F[k-1][V] ;
+			 XMax = 0;
+			 yk = min(dsdv[k].SL,V/dsdv[k].TL);
+			 for(xk = 1; xk<=yk; xk++)
+			 	if(F[k-1][V-xk*dsdv[k].TL]+xk*dsdv[k].GT>FMax){
+			 		FMax=F[k-1][V-xk*dsdv[k].TL]+xk*dsdv[k].GT;
+			 		XMax= xk;
+			 		}
+			 F[k][V] = FMax;
+			 X[k][V] = XMax;
+	 	}
+}
+
+void InBang(int n, int W, bang F, bang X){
+	int V, k;
+	for(k=0; k<n; k++){
+		for(V=0; V<=W; V++)
+		 	printf("|%3d%3d",F[k][V], X[k][V]);
+		printf("\n");
+	}
+}
+
+void TraBang(DoVat *dsdv, int n, int W, bang X) {
+	int k, V;
+	V = W;
+	for(k= n-1; k>=0; k--) {
+	 	dsdv[k].PA = X[k][V];
+	 	V = V - X[k][V] * dsdv[k].TL;
+	}
+}
+
+int main(){
+	int n, W;
+	bang X,F;
+	DoVat *dsdv;
+	dsdv=ReadFromFile(&W, &n);
+	
+	TaoBang(dsdv,n,W,F,X);
+	InBang(n,W,F,X);
+	printf("\n");
+	TraBang(dsdv,n,W,X);
+	InDSDV(dsdv,n,W);
+	
+	free(dsdv);
+	return 0;
+}
